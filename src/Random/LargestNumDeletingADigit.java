5f9d339f0854ef180e4d70994705db90122f74d9
mishraaditya595 <mishraaditya595@gmail.com>
2020-08-25 20:15:49 +0530

    Added LargestNumDeletingADigit.java

--- src/Random/LargestNumDeletingADigit.java
+++ src/Random/LargestNumDeletingADigit.java
@@ -1,17 +1,46 @@
 package Random;
 
-import java.util.Scanner;
+import java.util.*;
 
-public class StringPermutations {
+public class LargestNumDeletingADigit {
 
-    public static void main(String[] args) {
+    ArrayList<Integer> nList = new ArrayList<>();
+    Integer digit;
+
+    public void getInput(){
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter a string: ");
-        String str = in.nextLine();
-        permutation(str);
+        System.out.print("Enter a num: ");
+        String num = in.nextLine();
+        getArray(num);
+        System.out.print("Enter the digit to be removed: ");
+        digit = in.nextInt();
     }
 
-    private static void permutation(String str) {
-        for (int i = 0 )
+    public void getArray(String s){
+        for (char c : s.toCharArray())
+            nList.add(Character.getNumericValue(c));
+    }
+
+    public void getLargestNum(){
+        if (!nList.contains(digit))
+        {
+            System.out.println(digit+" is not present in the entered num.");
+            System.exit(0);
+        }
+        nList.remove(digit);
+        nList.sort(Collections.reverseOrder());
+    }
+
+    public void display(){
+        for (int i : nList)
+            System.out.print(i);
+    }
+
+
+    public static void main(String[] args) {
+        LargestNumDeletingADigit obj = new LargestNumDeletingADigit();
+        obj.getInput();
+        obj.getLargestNum();
+        obj.display();
     }
 }
