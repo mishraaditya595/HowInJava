79870bced25d86f6437ffeb60d0fd4c822e4a10d
mishraaditya595 <mishraaditya595@gmail.com>
2020-08-25 20:18:00 +0530

    minor

--- src/Random/LargestNumDeletingADigit.java
+++ src/Random/LargestNumDeletingADigit.java
@@ -1,3 +1,8 @@
+/*
+Java program to find the largest number ‘L’ less than a given number ‘N’ which should not contain a given digit ‘D’.
+For example, If 145 is the given number and 4 is the given digit, then you should find the largest number less than 145
+such that it should not contain 4 in it. In this case, 139 will be the answer.
+ */
 package Random;
 
 import java.util.*;
