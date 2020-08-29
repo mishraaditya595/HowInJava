44d43b2d10d99bdf71ab940db6a83d5c55f2ef48
mishraaditya595 <mishraaditya595@gmail.com>
2020-08-29 21:58:20 +0530

    Added SumOfDigitsDivBy10powN.java

--- src/Numbers/SumOfDigitsDivBy10powN.java
+++ src/Numbers/SumOfDigitsDivBy10powN.java
@@ -1,4 +1,43 @@
+/*
+Given a number N, find the smallest number such that the sum of its digits is N and it is divisible by 10^N .
+Input: 15, Output: 69000000000000000
+Input: 26, Output: 59900000000000000000000000
+ */
 package Numbers;
 
-public class Question1 {
+import java.util.Scanner;
+
+public class SumOfDigitsDivBy10powN {
+
+    public static int smallestNum(int n){
+        int x = 0, i = 1;
+        while (true){
+            x = getSumOfDigits(i);
+            if (x == n)
+                break;
+            i++;
+        }
+        return i;
+    }
+
+    private static int getSumOfDigits(int i) {
+        int sum = 0;
+        while (i > 0){
+            sum += i % 10;
+            i/=10;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int N = in.nextInt();
+        int small = smallestNum(N);
+        System.out.print(small);
+        while (N > 0) {
+            System.out.print("0");
+            N--;
+        }
+    }
 }
