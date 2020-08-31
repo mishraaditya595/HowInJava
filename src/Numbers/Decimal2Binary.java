36eb21e4277f368aa829a5192563adcf14165aaa
mishraaditya595 <mishraaditya595@gmail.com>
2020-08-31 09:49:04 +0530

    Added Decimal2Binary.java

---
+++ src/Numbers/Decimal2Binary.java
@@ -0,0 +1,28 @@
+/*
+Given an integer N as input in decimal format, convert it into binary without using an array.
+Input: 25
+Output: 11001
+ */
+package Numbers;
+
+import java.util.Scanner;
+
+public class Decimal2Binary {
+
+    public static int toBinary(int n){
+        String binary = "";
+        while (n>0){
+            int r = n % 2;
+            binary = r + binary;
+            n/=2;
+        }
+        return Integer.parseInt(binary);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a num: ");
+        int n = in.nextInt();
+        System.out.println("Binary: "+toBinary(n));
+    }
+}
